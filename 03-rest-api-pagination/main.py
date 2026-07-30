f45a1bc14f446990e262047ef825bd3e079ff066
dinevgit392 <dinevgit392@gmail.com>
2026-07-30 10:54:28 +0530

    Implement data fetching and saving from API
    Fetch data from REST API and save to JSON file.

--- 03-rest-api-pagination/main.py
+++ 03-rest-api-pagination/main.py
@@ -1,1 +1,22 @@
+import json
+import os
 
+from connectors.rest_connector import RestConnector
+
+connector = RestConnector(
+    base_url="https://jsonplaceholder.typicode.com/posts",
+    page_size=10
+)
+
+data = connector.fetch_data()
+
+os.makedirs("output", exist_ok=True)
+
+with open("output/posts.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+print()
+
+print(f"Total Records = {len(data)}")
+
+print("Saved Successfully")
